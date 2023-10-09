f53d4e29d0181f57a43bfae3d81688af02b2c75d
someone5678 <someone5678@users.noreply.github.com>
2023-10-09 22:05:37 +0900

    sweet-misound: Clean-ups
    Change-Id: Ie2614b2ba00d85a39dd1af60072ee03789627a63

--- MiSound.mk
+++ MiSound.mk
@@ -11,28 +11,27 @@ PRODUCT_SOONG_NAMESPACES += \
 # Exclude AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
 
-# Permissions & Sysconfig
+# Configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/system_ext/,$(TARGET_COPY_OUT_SYSTEM_EXT)/etc) \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/vendor/,$(TARGET_COPY_OUT_VENDOR)/etc) \
 
-# Properties {{{
+# Properties
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.audio.hifi=false \
-    ro.vendor.audio.misound.bluetooth.enable=true \
-    ro.vendor.audio.ring.filter=true \
-    ro.vendor.audio.sfx.earadj=true \
-    ro.vendor.audio.sfx.scenario=true \
-    ro.vendor.audio.soundfx.type=mi \
-    ro.vendor.audio.soundfx.usb=true
+ro.vendor.audio.hifi=false \
+ro.vendor.audio.misound.bluetooth.enable=true \
+ro.vendor.audio.ring.filter=true \
+ro.vendor.audio.sfx.earadj=true \
+ro.vendor.audio.sfx.scenario=true \
+ro.vendor.audio.soundfx.type=mi \
+ro.vendor.audio.soundfx.usb=true
 
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.dolby.dax.version=DAX3_3.6.1.6_r1 \
-    ro.vendor.audio.dolby.dax.support=true \
-    ro.vendor.audio.dolby.dax.version=DAX3_3.6 \
-    vendor.audio.dolby.ds2.enabled=true \
-    vendor.audio.dolby.ds2.hardbypass=false \
-    ro.audio.monitorRotation=true
-# }}}
+ro.vendor.dolby.dax.version=DAX3_3.6.1.6_r1 \
+ro.vendor.audio.dolby.dax.support=true \
+ro.vendor.audio.dolby.dax.version=DAX3_3.6 \
+vendor.audio.dolby.ds2.enabled=true \
+vendor.audio.dolby.ds2.hardbypass=false \
+ro.audio.monitorRotation=true
 
 $(call inherit-product, vendor/xiaomi/sweet-misound/common/common-vendor.mk)
